c1f4901e15666494e65a9f1e65ef53f5f2de5e08
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-08-26 22:04:26 -0400

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -1,3 +1,12 @@
 import os
 print()
 def main():
+    print("Welcome to pyTerm!")
+    print("Please wait while I set everything up...")
+    while True:
+        print()
+        inp = input("$ ")
+        if inp == "pico":
+            os.system('pico')
+        else:
+            print("Sorry, this is not a command :(")

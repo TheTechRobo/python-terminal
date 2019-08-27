8ec4b14669db79924b6bc4bcf7bb2bc22151876e
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-08-26 22:08:51 -0400

    Update terminal.py
    this should work in theroy

--- terminal.py
+++ terminal.py
@@ -8,5 +8,9 @@ def main():
         inp = input("$ ")
         if inp == "pico":
             os.system('pico')
+        elif "echo" in inp:
+            echoThis = input("Type what you want to echo: ")
+            os.system('echo ' +echoThis)
         else:
             print("Sorry, this is not a command :(")
+        print()

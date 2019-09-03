3b9c4348af1b1ddc6cccfe46b8df2b1e45199309
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-09-03 13:43:31 -0400

    pyTerm Commit
    • more support for pico
    • linux or mac question
    thanks for using!

--- terminal.py
+++ terminal.py
@@ -3,12 +3,18 @@ print()
 def main():
     print("Welcome to pyTerm!")
     print("Please wait while I set everything up...")
+    lin = input("python: are you running Linux or Mac? (yes / no, case-sensitive)")
+    if lin == "no":
+        print("python: this is useless to you")
     while True:
         print()
         inp = input("$ ")
-        if inp == "pico":
-            os.system('pico')
+        if "pico" in inp:
+            print("sorry, you cannot use arguments and parameters in pico right now :(")
+            picoThis = input("File-Name? ")
+            os.system('pico' +picoThis)
         elif "echo" in inp:
+            print("Sorry, you cannot use arguments and parameters in echo right now :(")
             echoThis = input("Type what you want to echo: ")
             os.system('echo ' +echoThis)
         else:
